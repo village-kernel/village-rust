a9c3ea5921bb22888686c44f06b937fc0894dd69
aicodeone <277579066@qq.com>
2025-06-03 21:28:20 +0800

    string：增加memmove

--- village_kernel/src/vklibs/libc/string.rs
+++ village_kernel/src/vklibs/libc/string.rs
@@ -14,15 +14,21 @@ pub unsafe extern "C" fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut
 }
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn memcmp(s1: *const u8, s2: *const u8, n: usize) -> i32 {
-    for i in 0..n {
-        let a = unsafe { *s1.add(i) };
-        let b = unsafe { *s2.add(i) };
-        if a != b {
-            return a as i32 - b as i32;
+pub unsafe  extern "C" fn memmove(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
+    if src < dest as *const _ && src.add(n) > dest as *const _ {
+        for i in (0..n).rev() {
+            unsafe {
+                *dest.add(i) = *src.add(i);
+            }
+        }
+    } else {
+        for i in 0..n {
+            unsafe {
+                *dest.add(i) = *src.add(i);
+            }
         }
     }
-    0
+    dest
 }
 
 #[unsafe(no_mangle)]
@@ -32,4 +38,16 @@ pub unsafe extern "C" fn memset(dest: *mut u8, c: i32, n: usize) -> *mut u8 {
         unsafe { *dest.add(i) = c; }
     }
     dest
+}
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn memcmp(s1: *const u8, s2: *const u8, n: usize) -> i32 {
+    for i in 0..n {
+        let a = unsafe { *s1.add(i) };
+        let b = unsafe { *s2.add(i) };
+        if a != b {
+            return a as i32 - b as i32;
+        }
+    }
+    0
 }
