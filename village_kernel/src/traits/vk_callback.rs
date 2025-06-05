a67608494321ee7490abe849f2ef44ed4b376ad1
aicodeone <277579066@qq.com>
2025-06-05 17:19:41 +0800

    vk_callback：完善回调逻辑

--- village_kernel/src/traits/vk_callback.rs
+++ village_kernel/src/traits/vk_callback.rs
@@ -8,21 +8,26 @@ extern crate alloc;
 use alloc::boxed::Box;
 use core::any::Any;
 
-// callback trait
+// Callback trait that all callable types must implement
 pub trait Callback: Any { }
 
-// typedef callback instance, function and method
-pub type Instance = Box<dyn Any>;
-pub type Function = Box<dyn for<'a> FnMut(&'a mut dyn Any, *mut ())>;
-pub type Method<T> = fn(&mut T, *mut ());
+// Type aliases for callback instance and function
+pub type CbInstance = Box<dyn Any>;
+pub type CbFunction = Box<dyn for<'a> FnMut(&'a mut dyn Any, *mut ())>;
 
-// erase user to instance
-pub fn erase_user_instance<T: 'static + Callback>(instance: T) -> Instance {
-    Box::new(instance)
+// Type aliases for function callback and method callback
+pub type FnCallback = fn(*mut ());
+pub type MethodCb<T> = fn(&mut T, *mut ());
+
+// Erase a function pointer to a callback function
+pub fn convert_fn_to_cb(func: FnCallback) -> CbFunction {
+    Box::new(move |_: &mut dyn Any, data_ptr| {
+        (func)(data_ptr);
+    })
 }
 
-// erase method to callback
-pub fn erase_method_callback<T: 'static + Callback>(method: Method<T>) -> Function {
+// Erase a method pointer to a callback function
+pub fn convert_method_to_cb<T: 'static + Callback>(method: MethodCb<T>) -> CbFunction {
     Box::new(move |obj: &mut dyn Any, data_ptr| {
         if let Some(concrete_obj) = obj.downcast_mut::<T>() {
             (method)(concrete_obj, data_ptr);
@@ -30,12 +35,43 @@ pub fn erase_method_callback<T: 'static + Callback>(method: Method<T>) -> Functi
     })
 }
 
-// invoke callback
-pub fn invoke_callback(callback: &mut Option<Function>, instance: &mut Option<Instance>, data: *mut ())
-{
-    if let (Some(ref mut callback), Some(ref mut instance)) =
-        (callback, instance)
-    {
-        callback(instance.as_mut(), data);
+// Structure to hold callback function, instance, and user data
+pub struct CbInvoker {
+    callback: Option<CbFunction>,
+    instance: Option<CbInstance>,
+    userdata: *mut (),
+}
+
+impl CbInvoker {
+    // Create a new empty callback invoker
+    pub fn new() -> Self {
+        Self {
+            callback: None,
+            instance: None,
+            userdata: core::ptr::null_mut(),
+        }
+    }
+
+    // Register a function pointer-based callback
+    pub fn register_fn(&mut self, func: FnCallback, data: *mut ()) {
+        self.callback = Some(convert_fn_to_cb(func));
+        self.instance = Some(Box::new(()));
+        self.userdata = data;
+    }
+
+    // Register a class method-based callback
+    pub fn register_method<T: 'static + Callback>(&mut self, instance: T, method: MethodCb<T>, data: *mut ()) {
+        self.callback = Some(convert_method_to_cb(method));
+        self.instance = Some(Box::new(instance));
+        self.userdata = data;
+    }
+
+    // Invoke the registered callback
+    pub fn invoke(&mut self) {
+        if let (Some(ref mut callback), Some(ref mut instance)) =
+            (&mut self.callback, &mut self.instance)
+        {
+            callback(instance.as_mut(), self.userdata);
+        }
     }
 }
