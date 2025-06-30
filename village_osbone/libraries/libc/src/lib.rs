490972407d09284320030add370d8278fa09ffaf
aicodeone <277579066@qq.com>
2025-07-01 01:57:49 +0800

    libc：初步增加libc库

---
+++ village_osbone/libraries/libc/src/lib.rs
@@ -0,0 +1,11 @@
+//###########################################################################
+// lib.rs
+// The specific implementation of functions related to lib
+//
+// $Copyright: Copyright (C) village
+//###########################################################################
+#![no_std]
+#![no_main]
+
+// Import string
+pub mod string;
