974606bf6d0e8f509ebbd345e8b485bf0e444a70
aicodeone <277579066@qq.com>
2025-07-17 23:16:34 +0800

    libc：修改库编译配置

--- village_osbone/libraries/libc/src/lib.rs
+++ village_osbone/libraries/libc/src/lib.rs
@@ -18,8 +18,34 @@ unsafe extern "C" {
     unsafe fn abort() -> !;
 }
 
-// Panic
+// panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     unsafe { abort() }
+}
+
+// Export marco
+#[macro_export]
+macro_rules! export {
+    ($fn:item) => {
+        #[unsafe(link_section = ".exported_symbols")]
+        #[unsafe(no_mangle)]
+        $fn
+    };
+}
+
+// Export impl marco
+#[macro_export]
+macro_rules! export_impl {
+    ($ty:ty, $method:ident, $name:ident, $($args:ty),*) => {
+        #[unsafe(link_section = ".exported_symbols")]
+        #[unsafe(no_mangle)]
+        pub extern "C" fn $name(ptr: *mut $ty, $($args),*) {
+            unsafe {
+                if let Some(obj) = ptr.as_mut() {
+                    obj.$method($($args),*);
+                }
+            }
+        }
+    };
 }
--- village_osbone/libraries/libc/src/string.rs
+++ village_osbone/libraries/libc/src/string.rs
@@ -4,50 +4,59 @@
 //
 // $Copyright: Copyright (C) village
 //###########################################################################
+use crate::export;
 
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
-    for i in 0..n {
-        unsafe { *dest.add(i) = *src.add(i) };
-    }
-    dest
-}
-
-#[unsafe(no_mangle)]
-pub unsafe  extern "C" fn memmove(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
-    if src < dest as *const _ && unsafe { src.add(n) } > dest as *const _ {
-        for i in (0..n).rev() {
-            unsafe {
-                *dest.add(i) = *src.add(i);
-            }
-        }
-    } else {
+export! {
+    // memcpy
+    pub extern "C" fn memcpy(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
         for i in 0..n {
-            unsafe {
-                *dest.add(i) = *src.add(i);
+            unsafe { *dest.add(i) = *src.add(i) };
+        }
+        dest
+    }
+}
+
+export! {
+    // memmove
+    pub extern "C" fn memmove(dest: *mut u8, src: *const u8, n: usize) -> *mut u8 {
+        if src < dest as *const _ && unsafe { src.add(n) } > dest as *const _ {
+            for i in (0..n).rev() {
+                unsafe {
+                    *dest.add(i) = *src.add(i);
+                }
+            }
+        } else {
+            for i in 0..n {
+                unsafe {
+                    *dest.add(i) = *src.add(i);
+                }
             }
         }
+        dest
     }
-    dest
 }
 
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn memset(dest: *mut u8, c: i32, n: usize) -> *mut u8 {
-    let c = c as u8;
-    for i in 0..n {
-        unsafe { *dest.add(i) = c; }
-    }
-    dest
-}
-
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn memcmp(s1: *const u8, s2: *const u8, n: usize) -> i32 {
-    for i in 0..n {
-        let a = unsafe { *s1.add(i) };
-        let b = unsafe { *s2.add(i) };
-        if a != b {
-            return a as i32 - b as i32;
+export! {
+    // memset
+    pub extern "C" fn memset(dest: *mut u8, c: i32, n: usize) -> *mut u8 {
+        let c = c as u8;
+        for i in 0..n {
+            unsafe { *dest.add(i) = c; }
         }
+        dest
     }
-    0
+}
+
+export! {
+    // memcmp
+    pub extern "C" fn memcmp(s1: *const u8, s2: *const u8, n: usize) -> i32 {
+        for i in 0..n {
+            let a = unsafe { *s1.add(i) };
+            let b = unsafe { *s2.add(i) };
+            if a != b {
+                return a as i32 - b as i32;
+            }
+        }
+        0
+    }
 }
